daa9efe0a282619db165f54190ff866dbe7af9ff
dynuaa <dynuaa@gmail.com>
2012-05-14 17:00:06 +0800

    SharedSync: general update

--- Python/SharedSync/shared_sync.py
+++ Python/SharedSync/shared_sync.py
@@ -7,15 +7,19 @@ magic='--calling-python-from-/bin/sh--'
 # Specify the repo list you do NOT want to link them to common project folder.
 # Typically, they are the repositories to which you often makes changes.
 # The path name can be found in .repo/manifest.xml
-EXCLUDED_PROJECT_LIST = ("packages/apps/Settings",
-                         "motorola/packages/blur")
+EXCLUDED_PROJECT_LIST = ("packages/apps/Settings", "motorola/packages/blur")
 
 # Specify the path where to store the shared projects. Must be absolute path.
-SHARED_PROJECTS_PATH = '/localrepo/a18238/shared_projects'
+SHARED_PROJECTS_PATH = '/localrepo/<core-id>/shared_projects'
 
 
 import os
 from xml.etree import ElementTree
+
+
+def printUsage():
+    print "shared-sync v1.0"
+
 
 # Make sure the common shared folder is created
 def createSharedProjsFolder(xml_projects):
@@ -73,10 +77,14 @@ def getSharedProjsPath():
     global gSharedProjectsPath
 
     if gSharedProjectsPath == None:
-        gSharedProjectsPath = SHARED_PROJECTS_PATH
-        if gSharedProjectsPath == None:
-            print 'Error: invalid SHARED_PROJECTS_PATH!'
+        if SHARED_PROJECTS_PATH == '/localrepo/<core-id>/shared_projects' or \
+        SHARED_PROJECTS_PATH == None or \
+        SHARED_PROJECTS_PATH == '':
+            print 'Error: invalid SHARED_PROJECTS_PATH! Update SHARED_PROJECTS_PATH in the script!'
+            printUsage()
             raise InvalidProjectsPathError()
+
+        gSharedProjectsPath = SHARED_PROJECTS_PATH
         if gSharedProjectsPath[-1] != '/':
             gSharedProjectsPath += '/'
     return gSharedProjectsPath
