a8a1be1968fc50ca86f68b363df703964da78fd7
dynuaa <dynuaa@gmail.com>
2012-12-29 19:40:46 +0800

    Apps/HotSquare: minor

--- Apps/HotSquare/HotSquare.h
+++ Apps/HotSquare/HotSquare.h
@@ -7,8 +7,11 @@
 // to save debug time, define it to 0 to read all segments
 #define SEGMENTS_CSV_READ_LIMIT  0//10000
 
-#define LAT_METERS_PER_DEGREE   65355
-#define LNG_METERS_PER_DEGREE   111190
+// Below two defines are specific to Harbin
+// Refer to http://www.hhlink.com/%E7%BB%8F%E7%BA%AC%E5%BA%A6
+//          http://blog.csdn.net/e_wsq/article/details/7551376
+#define LAT_METERS_PER_DEGREE   111190
+#define LNG_METERS_PER_DEGREE   77628
 
 
 #define SEGMENTS_CSV_PATH   "Data\\WAY_SEGMENTS\\data"
